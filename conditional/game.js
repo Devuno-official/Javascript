90970ad5f24095bac55c56978d85ac7347a3787e
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-05-05 18:46:42 +0500

    some uniqe change

--- conditional/game.js
+++ conditional/game.js
@@ -7,21 +7,23 @@
  *
 */
  // do with for-while statement
-  var userNum = prompt("solve a puzzula, enter the correct number");
-  let gameNum = 25 ;
-  while(userNum != gameNum){
-     userNum = prompt("Sorry , this is not a correct number , try again");
-  }
-        console.log("congratulations , You solve this puzzula");
+//   var userNum = prompt("solve a puzzula, enter the correct number");
+//   let gameNum = 25 ;
+//   while(userNum != gameNum){
+//      userNum = prompt("Sorry , this is not a correct number , try again");
+//   }
+//         console.log("congratulations , You solve this puzzula");
 
-
-s
 
 // // do this with if statement 
-// let x = prompt("solve a puzzula, enter the correct number");
-//  if (x == 5  || x == 25 || x == 50 || x == 75 || x == 99) {
-//                    console.log("wow,YOU break the Puzzula");
-//  }else{
-//           console.warn("Sorry,this is not a correct value,try again");
-//  }
-//       console.trace("thanks for playing game");
+ let main = document.querySelectorAll("#main");
+
+let x = prompt("solve a puzzula, enter the correct number");
+      if (x == 5  || x == 25 || x == 50 || x == 75 || x == 99) {
+                   console.log("wow,YOU break the Puzzula");
+  }else{
+           console.warn("Sorry,this is not a correct value,try again\n");
+           console.trace("thanks for playing game");
+  }
+         console.dir(main);
+
