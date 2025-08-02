bbad731d32020fc858dc6011a1ceaf2a6f8d8e97
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-08-02 23:00:33 +0500

    local storage bug fixed

--- Dom/toggleclick/script.js
+++ Dom/toggleclick/script.js
@@ -1,15 +1,15 @@
   let btn = document.querySelector("#btn");
  let body = document.body;
    
- if(localStorage.getItem("darkmode") === 'true'){
+ if(JSON.parse(localStorage.getItem("darkmode")) === true){
          body.classList.add("mode");
  }
 
     btn.addEventListener("click",(ev)=>{
-      ev.preventDefault()
-       body.classList.toggle("mode");   
+      ev.preventDefault() 
+        body.classList.toggle("mode");
        let isDark = body.classList.contains("mode");
-        localStorage.getItem("darkmode", isDark);
+        localStorage.setItem("darkmode",JSON.stringify(isDark));
     });
 
   
