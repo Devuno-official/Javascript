a18d7a87e248695d41eea9f3e9dffcc000498e55
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-08-07 22:12:15 +0500

    Date object in js

---
+++ Datesobject/Datemothed.js
@@ -0,0 +1,30 @@
+
+let date = new Date();
+console.log(date.toUTCString());  // * iso standard 2024-02-12T05:00:00z
+
+
+
+//* get minutes is a method that get minutes for lastest time 
+let minutes = date.getMinutes();
+ console.log(minutes,'min');
+ 
+//* get second is a method that get minutes for lastest time
+ let second = date.getSeconds();
+ console.log(second,'sec');
+
+
+ //* get minutes is a method that get minutes for lastest time
+ let hours = date.getHours();
+ console.log(hours,'h');
+ 
+ 
+ // *get day is a method that get minutes for lastest time
+ let day = date.getDay();
+ console.log(day,'day');
+ 
+
+ // *get date is a method that get minutes for lastest time
+ let timeDate = date.getDate();
+ console.log(timeDate,'date');
+ 
+ 
---
+++ Datesobject/firstDate.js
@@ -0,0 +1,16 @@
+// * the unix epoch is starting point 
+
+let date = new Date(0);
+console.log(date.toString());
+
+//   output
+//*  jan 01 1970 05:00:00 
+
+
+let ms = Date.now();
+ console.log(ms);
+ 
+ 
+// millisecond since  unix epoch
+// output
+//* ms = 1754581953643643
