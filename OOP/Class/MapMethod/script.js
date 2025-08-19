1952723a5412d4e00694988e8a3d795529aee691
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-08-19 23:41:07 +0500

    Oop concept with bugfix branch

---
+++ OOP/Class/MapMethod/script.js
@@ -0,0 +1,27 @@
+
+// * A alternative maping class that provide us mapping method 
+
+
+class alternative{
+    map(arr , fn){
+          let arr2 = [];
+          for(let i = 0 ; i < arr.length ; i++){
+               arr2.push(fn(arr[i],i));
+          }
+          return arr2
+    }
+}
+
+let arr = [1,2,3,4,5];
+
+let obj = new alternative();
+
+ let PlusI =obj.map(arr, (val , i)=>{
+       return val + i;  
+});
+
+
+console.log(PlusI);
+
+
+
