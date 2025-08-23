101eb835f1aa7af8823eb96c44c3122d0bdc6466
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-08-23 21:04:27 +0500

    A simple todo list but js logic is not correct

--- Dom/to-do-list/script.js
+++ Dom/to-do-list/script.js
@@ -1,48 +1,43 @@
  let addNote = document.getElementById("addBtn");
+ let NoteDiolog= document.getElementById("diolog");
 
 addNote.addEventListener("click",function(event){
      event.preventDefault();
-     let NoteDiolog= document.getElementById("diolog");
-         if(NoteDiolog.style.display === 'none'){
-               NoteDiolog.style.display = 'flex';
-         }else{
-          NoteDiolog.style.display = 'none';
-         }
-     document.getElementById("cancelBtn").addEventListener("click",()=>{
-  if(NoteDiolog.style.display === 'flex') 
-     NoteDiolog.style.display = 'none';
+    if(NoteDiolog.style.display === 'none'){
+        NoteDiolog.style.display = 'flex';
+    }else{
+        NoteDiolog.style.display = 'none';
+    }
+       document.getElementById("Textarea").value ="";
+    document.getElementById("cancelBtn").addEventListener("click",()=>{
+               if(NoteDiolog.style.display === 'flex'){ 
+               NoteDiolog.style.display = 'none';
+            }
+    });
+              saveNoteProcess();
+                   return;    
 });
-    saveNoteProcess(NoteDiolog);
-     return;    
-});
+
+// romoveing bankSpace
+let TextTrim = function(){
+ let textVal = document.getElementById("Textarea");
+         return textVal.value.trim();
+}
 
 
-        let applyBtn =  document.getElementById("applyBtn");
- function saveNoteProcess(NoteDiolog){
-     let text = document.getElementById("Textarea");
-
-      applyBtn.addEventListener("click",(event)=>{    
-       let note = document.createElement("div");
-       document.getElementById("notes").appendChild(note);
-        note.classList.add("task");
-      let trimText = text.value.trim();
-          note.innerText = trimText;
-         text.value ="";
-     NoteDiolog.style.display = "none";
-
-     note.forEach((singlenote) => {
-          if(singlenote.value ===""){
-               singlenote.remove();
-          }
-     });
-        
- });
-           return;   
- }
-
-
- applyBtn.addEventListener("ketpress",(event)=>{
-       if(event.key === "Enter"){
-          saveNoteProcess();
-       }
- });
+function saveNoteProcess(){
+        //apply Btn
+    document.getElementById("applyBtn").addEventListener("click",function(){
+        let notediv = document.createElement("aside");
+        let addNote = function(){
+        notediv.classList.add('task');
+             return document.getElementById("notes").append(notediv);
+    }
+                notediv.innerText = TextTrim();
+            if(notediv.value === ''){
+                 notediv.remove();
+            }
+                NoteDiolog.style.display='none';
+                return addNote();
+    });
+}
