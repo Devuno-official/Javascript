aff832189d74db33209cedbbbe99ef9e9b11db84
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-09-05 17:57:26 +0500

    Dog Api with random image of dogs

--- FatchApi/DogRandomPicApi/DogAPi.js
+++ FatchApi/DogRandomPicApi/DogAPi.js
@@ -1,28 +1,22 @@
-let url = "https://dog.ceo/api/breeds/image/random";
 
-const dogPic = async function (url) {
+let url = "https://dog.ceo/api/breeds/image/random";
+let button = document.querySelector("button");
+let main = document.querySelector(".main");
+
+button.addEventListener("click", async function(dot) {
+  dot.stopPropagation();
   try {
     let Response = await fetch(url, {
       method: "GET",
     });
-
     if (!Response.ok) {
       throw new Error("Response is not ok : ", Response.status);
     } else {
       let data = await Response.json();
       let image = await data.message;
-      let button = document.querySelector("button");
-      button.addEventListener("click", function () {
-        let main = document.querySelector(".main");
-        main.style.backgroundImage = 'url("' + image + '")';
-        main.style.backgroundSize = "cover";
-        main.style.backgroundPosition = "center";
-        main.style.backgroundRepeat = "norepeat";
-      });
+      main.style.backgroundImage = 'url("' + image + '")';
     }
   } catch (error) {
     console.log(`Error : ${error}`);
   }
-};
-
-dogPic(url);
+});
